ab9dfce461665c5df5db954f700682e80ed7fdcc
Selesito <selesitomorozov@gmail.com>
2021-08-17 10:06:39 +0400

    create job4j_microservices

---
+++ src/main/java/ru/job4j/job4j_microservices/Job4jMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.job4j_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jMicroservicesApplication.class, args);
+    }
+
+}
